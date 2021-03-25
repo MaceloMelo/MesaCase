ecd25566a0bb6d326e90c6262015ed4e4c5804ae
MACELO MELO D <39223737+MaceloMelo@users.noreply.github.com>
2021-03-25 17:42:39 -0300

    make it better

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 
 module.exports = {
   devServer: {
-    proxy: 'https://maps.googleapis.com'
+    proxy: {
+      '^/api': {
+        target: 'https://maps.googleapis.com/',
+        ws: true,
+        changeOrigin: true
+      },
+    }
   }
 }
