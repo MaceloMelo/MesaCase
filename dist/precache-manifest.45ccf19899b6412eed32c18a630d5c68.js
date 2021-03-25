fbc51c83249657d938072bb76f22ca7e02ce1cbf
MACELO MELO D <39223737+MaceloMelo@users.noreply.github.com>
2021-03-25 19:16:04 -0300

    make it better

--- dist/precache-manifest.45ccf19899b6412eed32c18a630d5c68.js
+++ dist/precache-manifest.45ccf19899b6412eed32c18a630d5c68.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5aa9d338562b4d7600f3",
-    "url": "/css/about.bd808f2b.css"
+    "revision": "81ea96685ade34ad30e9",
+    "url": "/css/about.813e70af.css"
   },
   {
-    "revision": "6721b90a93d1746acb35",
+    "revision": "ad1d34a15431fec403f7",
     "url": "/css/app.ca936927.css"
   },
   {
@@ -20,16 +20,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/img/mesa-logo.71007257.png"
   },
   {
-    "revision": "7a86579c088082827b94894a80bafe24",
+    "revision": "797d2c015d963c1624fb038310a41473",
     "url": "/index.html"
   },
   {
-    "revision": "5aa9d338562b4d7600f3",
-    "url": "/js/about.46fb225b.js"
+    "revision": "81ea96685ade34ad30e9",
+    "url": "/js/about.94e4612b.js"
   },
   {
-    "revision": "6721b90a93d1746acb35",
-    "url": "/js/app.9897a0e8.js"
+    "revision": "ad1d34a15431fec403f7",
+    "url": "/js/app.0c10902d.js"
   },
   {
     "revision": "1039cf1dae17c5b4fc87",
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bdc1cf7e0aef38a01ad7cc8af5ea89b1.js"
+  "/precache-manifest.45ccf19899b6412eed32c18a630d5c68.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "googleplaces"});
