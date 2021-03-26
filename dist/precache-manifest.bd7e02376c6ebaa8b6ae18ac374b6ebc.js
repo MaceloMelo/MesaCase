2497f390842075fb0da242a94645f83cfb0702c8
MACELO MELO D <39223737+MaceloMelo@users.noreply.github.com>
2021-03-25 22:56:03 -0300

    make it better

---
+++ dist/precache-manifest.bd7e02376c6ebaa8b6ae18ac374b6ebc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22500309c0c15de2501a",
+    "url": "/css/about.5a74f758.css"
+  },
+  {
+    "revision": "3b84da993bca94df1b1e",
+    "url": "/css/app.ca936927.css"
+  },
+  {
+    "revision": "4c03c10e623ffca0545b",
+    "url": "/css/chunk-vendors.8cecb73c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "710072571d515189e21ffc733752fa93",
+    "url": "/img/mesa-logo.71007257.png"
+  },
+  {
+    "revision": "90fdac726537ff95d427c818894796a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22500309c0c15de2501a",
+    "url": "/js/about.cf654fdf.js"
+  },
+  {
+    "revision": "3b84da993bca94df1b1e",
+    "url": "/js/app.dce4cd57.js"
+  },
+  {
+    "revision": "4c03c10e623ffca0545b",
+    "url": "/js/chunk-vendors.01eaf6e5.js"
+  },
+  {
+    "revision": "c1cfe588fe3361112eb1ba588cdf8f4b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
