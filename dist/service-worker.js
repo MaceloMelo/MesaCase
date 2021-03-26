71ec5d301439e6f61dc64393da380232babfd428
MACELO MELO D <39223737+MaceloMelo@users.noreply.github.com>
2021-03-25 22:25:09 -0300

    make it better

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bd7e02376c6ebaa8b6ae18ac374b6ebc.js"
+  "/precache-manifest.d4585b3ef8df7888ab46379ecf1994c1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "googleplaces"});
--- src/http/axios.js
+++ src/http/axios.js
@@ -1,5 +1,9 @@
 import Vue from "vue";
 import axios from "axios";
+axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET';
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Content-Type';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'https://mgoogleplace.herokuapp.com';
 Vue.use({
   install(Vue) {
     Vue.prototype.$http = axios.create({
